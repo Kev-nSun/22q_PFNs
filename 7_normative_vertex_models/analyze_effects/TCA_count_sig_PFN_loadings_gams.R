7677c368df4fe92c48071f2de3d91fc6ee6ba704
Kevin Sun <112665202+Kev-nSun@users.noreply.github.com>
2026-09-22 16:50:07 -0400

    Add files via upload

--- 7_normative_vertex_models/analyze_effects/TCA_count_sig_PFN_loadings_gams.R
+++ 7_normative_vertex_models/analyze_effects/TCA_count_sig_PFN_loadings_gams.R
@@ -8,7 +8,7 @@ dir.create(dirout)
 var_verts <- read.csv("C:/Users/kevin/OneDrive/Documents/NGG_PhD/Alexander-Bloch/22q_Project/Variable_vertices_PFN_masks/PFN_var_verts.csv")
 
 #COUNT NUMBER OF SIG VERTICES, FDR THRESHOLDED ACROSS ALL NETWORKS:
-sig_mat <- matrix(NA_real_, 17, 8)
+sig_mat <- matrix(NA_real_, 17, 9)
 
 for (PFN in 1:17) {
   
@@ -37,6 +37,12 @@ for (PFN in 1:17) {
   pos_sig_verts <- sum(sig & beta > 0, na.rm = TRUE)
   neg_sig_verts <- sum(sig & beta < 0, na.rm = TRUE)
   
+  # Mean beta among all FDR-significant vertices
+  mean_sig_beta <- if (sig_verts > 0) {
+    mean(beta[sig], na.rm = TRUE)
+  } else {
+    NA_real_
+  }
   
   sig_mat[PFN, 1] <- n_var
   sig_mat[PFN, 2] <- sig_verts
